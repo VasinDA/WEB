1f72a13d36db348194a16195aa654ff3e96c5227
Denis Vasin <svga.shop@gmail.com>
2022-07-05 18:26:53 +0300

    blog

--- blog/app.py
+++ blog/app.py
@@ -2,14 +2,20 @@ from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
-@app.route('/home/')
+app.route('/')
+@app.route('/home')
 def home():
-    return render_template('base.html', title='Home page', header='Welcome to my blog', 
+    return render_template('base.html', header='Welcome to my blog', 
     content_header='About my blog')
+
+@app.route('/posts')
+def allposts():
+    return render_template('base.html', title='Posts page', header='Posts', 
+    content_header='All posts')
 
 @app.route('/posts/<data>')
 def postsbydata(data):
-    return render_template('base.html', title='Posts page', header='Posts', 
+    return render_template('base.html', title='Posts page', header='Posts {}'.format(data), 
     content_header='2022-10-12')
 
 @app.route('/post/my_post.html')
