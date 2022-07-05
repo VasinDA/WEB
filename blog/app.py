17e96e7e5bd9c4c1c6c474061e84b1d485f7a009
Eugene Lagoda <eugene.lagoda@gmail.com>
2022-07-05 09:40:45 -0700

    fix

--- blog/app.py
+++ blog/app.py
@@ -2,8 +2,8 @@ from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
-app.route('/')
-@app.route('/home')
+@app.route('/')
+@app.route('/home/')
 def home():
     return render_template('base.html', header='Welcome to my blog', 
     content_header='About my blog')
