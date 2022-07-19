968be34456231e08f0bfb6b3106686b7e2fc19e3
Eugene Lagoda <eugene.lagoda@gmail.com>
2022-07-19 09:47:28 -0700

    news - works with DB

--- blog/news.py
+++ blog/news.py
@@ -1,6 +1,14 @@
+import sqlite3
 class News:
     def __init__(self):
         self.news = []
+        self.connect = sqlite3.connect('data.db')
+        self.cursor = self.connect.cursor()
+
+        self.create_table()
+
+    def __del__(self):
+        self.connect.close()
     
     def getList(self, date = None):
         news = self.news.copy()
@@ -10,3 +18,14 @@ class News:
 
     def addNews(self, title, date, body):
         self.news.append({'title': title, 'date': date, 'body': body})
+
+    def create_table(self):
+        sql ="""
+        CREATE TABLE IF NOT EXISTS user (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            email TEXT,
+            password TEXT
+        );
+        """
+        self.cursor.executescript(sql)  
+
