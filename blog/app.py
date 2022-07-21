13276e185d056235e3a8ec970faa109e41ed9f5d
Denis Vasin <svga.shop@gmail.com>
2022-07-21 22:40:38 +0300

    blog database

--- blog/app.py
+++ blog/app.py
@@ -3,28 +3,27 @@ from blogs import Blog
 from news import News
 
 app = Flask(__name__)
-posts = Blog()
+blog = Blog()
 news = News()
 
 @app.route('/')
 @app.route('/home/')
 def home():
-    return render_template('home.html', title='Posts page', header='Welcome to my blog', posts=posts.getPosts())
+    return render_template('home.html', title='Posts page', header='Welcome to my blog', posts=blog.getPosts())
 
 # TODO: post should added by the route '/post/' with method POST.
-@app.route('/home/', methods=['POST'])
+@app.route('/posts/', methods=['POST'])
 def blog_add():
     # TODO: would be good to return a new post from 'add' method if we'd like to use some data.
-    posts.addPost(request.form.get('title', ''), request.form.get('date', ''), request.form.get('body', ''))
-    return redirect(url_for('postsbydate', date=request.form.get('date')))
+    return redirect(url_for('postsbydate', date=blog.addPost(request.form.get('title', ''), request.form.get('date', ''), request.form.get('body', ''))))
 
 @app.route('/posts/<date>')
 def postsbydate(date):
-    return render_template('posts.html', title='Posts page', header='Posts ' + date, posts=posts.getPostsByDate(date))
+    return render_template('posts.html', title='Posts page', header='Posts ' + date, posts=blog.getPostsByDate(date))
 
 @app.route('/post/<sku>.html')
 def post(sku):
-    return render_template('post.html', title='Post page', header=sku, post=posts.getPostBySku(sku))
+    return render_template('post.html', title='Post page', header=sku, post=blog.getPostBySku(sku))
 
 @app.route('/news')
 def news_list():
--- blog/blogs.py
+++ blog/blogs.py
@@ -1,20 +1,19 @@
 import sqlite3
 class Blog:
     def __init__(self):
-        self.connect = sqlite3.connect('data.db')
+        self.connect = sqlite3.connect('data.db', check_same_thread=False)
         self.cursor = self.connect.cursor()
         self.create_table()
 
     def create_table(self):
         sql = """
         CREATE TABLE IF NOT EXISTS posts (
-            id INTEGER AUTOINCREMENT,
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
             sku TEXT,
             title TEXT,
             date TEXT,
             body TEXT
-            CONSTRAINT PK_posts PRIMARY KEY (id,sku)
-        );
+            );
         """
         self.cursor.executescript(sql)
 
@@ -22,34 +21,34 @@ class Blog:
         self.connect.close() 
 
     def addPost(self, title, date, body):
-        title = str(title).capitalize()
-        sku = title.replace(' ', '_')
+        sku = title.replace(' ', '_').lower()
         date = {'id': None, 'sku': sku, 'title': title, 'date': date, 'body': body}
         sql = 'INSERT INTO posts VALUES(:id, :sku, :title, :date, :body);'
         self.cursor.execute(sql, date)
         self.connect.commit()
-        
+        sql = 'SELECT date FROM posts ORDER BY ID DESC LIMIT 1'
+        self.cursor.execute(sql)
+        for date in self.cursor:
+            date, = date
+        return date
+       
     def getPosts(self, count = 5):
         sql = 'SELECT title, date, body FROM posts ORDER BY date LIMIT ?;'
-        self.cursor.execute(sql, str(count))
-        posts = [{'title': title, 'date': date, 'body': body} for title, date, body in self.cursor]
+        posts = self.getDateFromDataBase(sql, count)
         return posts
 
     def getPostsByDate(self, date):
         sql = 'SELECT title, date, body FROM posts WHERE date=?;'
-        date = [date]
-        self.cursor.execute(sql, date)
-        posts = [{'title': title, 'date': date, 'body': body} for title, date, body in self.cursor]
+        posts = self.getDateFromDataBase(sql, date)
         return posts
 
     def getPostBySku(self, sku):
         sql = 'SELECT title, date, body FROM posts WHERE sku=? LIMIT 1;'
-        sku = [sku]
-        self.cursor.execute(sql, sku)
-        post = [{'title': title, 'date': date, 'body': body} for title, date, body in self.cursor]
+        post = self.getDateFromDataBase(sql, sku)
         return post
 
-
-p = Blog()
-p.addPost('Post 1', '2020-02-02', 'Post2')
-print(p.getPosts())
+    def getDateFromDataBase(self, sql, date):
+        date = [date]
+        self.cursor.execute(sql, date)
+        posts = [{'title': title, 'date': date, 'body': body} for title, date, body in self.cursor]
+        return posts
